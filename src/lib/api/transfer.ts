66a3aaea5179bbe765ef91157bdd89c402114b95
seunghyonpark <creath.park@gmail.com>
2025-05-31 12:19:11 +0900

    Refactor: Update database collection names from 'shinemywinter' to 'vienna'

--- src/lib/api/transfer.ts
+++ src/lib/api/transfer.ts
@@ -39,11 +39,11 @@ export async function insertOne(data: any) {
     // if toAddress is user wallet address, then insert into userTransfers collection
 
 
-    const collectionUsers = client.db('shinemywinter').collection('users');
+    const collectionUsers = client.db('vienna').collection('users');
 
-    const collectionUserTransfers = client.db('shinemywinter').collection('userTransfers');
+    const collectionUserTransfers = client.db('vienna').collection('userTransfers');
 
-    const collection = client.db('shinemywinter').collection('transfers');
+    const collection = client.db('vienna').collection('transfers');
 
 
     
@@ -128,7 +128,7 @@ export async function insertOne(data: any) {
 
 
 
-            const collectionTelegramMessages = client.db('shinemywinter').collection('telegramMessages');
+            const collectionTelegramMessages = client.db('vienna').collection('telegramMessages');
 
             await collectionTelegramMessages.insertOne(
             {
@@ -165,7 +165,7 @@ export async function getTransferByWalletAddress(data: any) {
 
     const client = await clientPromise;
 
-    const collectionUsers = client.db('shinemywinter').collection('users');
+    const collectionUsers = client.db('vienna').collection('users');
 
     
     const user = await collectionUsers.findOne(
@@ -182,7 +182,7 @@ export async function getTransferByWalletAddress(data: any) {
     // timestamp desc
     
 
-    const collectionUserTransfers = client.db('shinemywinter').collection('userTransfers');
+    const collectionUserTransfers = client.db('vienna').collection('userTransfers');
 
     const userTransfers = await collectionUserTransfers
     .find({ "user.walletAddress": data.walletAddress })
